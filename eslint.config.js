89442973151e8e6c9d9bbf4f0bc8f0d286beedf8
zenlab <penzel5@web.de>
2026-03-25 17:44:15 +0100

    CI fixes

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,15 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
 
 export default [
   { ignores: ['dist', 'node_modules', 'src-tauri/target', 'coverage'] },
   {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
+      parser: tsparser,
       ecmaVersion: 2020,
       globals: {
         ...globals.browser,
@@ -20,17 +23,19 @@ export default [
       },
     },
     plugins: {
+      '@typescript-eslint': tseslint,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
-      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-unused-vars': 'off',
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
     },
